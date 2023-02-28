f1f88b93a55de97a3995a69c6ed5d0bb8740b9f4
enoqueJonas <enoque.macanda@gmail.com>
2023-02-28 17:43:02 +0200

    FIX linters

--- game.rb
+++ game.rb
@@ -1,20 +1,21 @@
-require_relative './item.rb'
+require_relative './item'
 require 'date'
 
 class Game < Item
-    attr_accessor :multiplayer, :last_played_at
+  attr_accessor :multiplayer, :last_played_at
 
-    def initialize(genre = "Unknown", author= "Unknown", source= "Unknown", label= "Unknown", publish_date = "2022-02-15", multiplayer, last_played_at)
-        super(genre, author, source, label, publish_date)
-        @multiplayer = multiplayer
-        @last_played_at = Date.parse(last_played_at)
-    end
+  def initialize(game_info, multiplayer, last_played_at)
+    super(game_info.genre, game_info.author, game_info.source, game_info.label, game_info.publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = Date.parse(last_played_at)
+  end
 
-    def can_be_archived?
-        current_year = DateTime.now
-        current_year = current_date.strftime "%Y"
-        age = current_year - last_played_at.year
-        return true if super && age > 2
-        false
-    end
+  def can_be_archived?
+    current_year = DateTime.now
+    current_year = current_year.strftime '%Y'
+    age = current_year - last_played_at.year
+    return true if super && age > 2
+
+    false
+  end
 end
