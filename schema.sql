c197310f42ccfb15cc2148e455e0c4099301a02a
SimonGideon <simongideon918@gmail.com>
2023-03-01 00:22:52 +0300

    Add Album_genre sql schema

--- schema.sql
+++ schema.sql
@@ -3,6 +3,8 @@ CREATE DATABASE catalogs;
 CREATE TABLE item(
     id int NOT NULL PRIMARY KEY,
     author_id int NOT NULL REFERENCES author(id) ON DELETE cascade
+    genre_id int NOT NULL,
+    FOREIGN KEY (genre_id) REFERENCES genre(genre_id),
 );
 
 CREATE TABLE game(
@@ -15,4 +17,14 @@ CREATE TABLE author(
     id INT NOT NULL PRIMARY KEY,
     first_name varchar(50),
     last_name varchar(50)
+);
+
+CREATE TABLE genre(
+    genre_id INT IDENTITY(1,1) PRIMARY KEY,
+    name varchar(50)
+);
+
+CREATE TABLE music_album(
+    album_id INT IDENTITY(1,1) PRIMARY KEY,
+    on_spotify boolean,
 );
