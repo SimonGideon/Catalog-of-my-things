9734ee8a77b9c6c0a4a75251988b7132f4f4cda9
enoqueJonas <enoque.macanda@gmail.com>
2023-02-28 17:26:50 +0200

    UPDATE initialize in game.rb

--- game.rb
+++ game.rb
@@ -4,7 +4,8 @@ require 'date'
 class Game < Item
     attr_accessor :multiplayer, :last_played_at
 
-    def initialize(multiplayer, last_played_at)
+    def initialize(genre = "Unknown", author= "Unknown", source= "Unknown", label= "Unknown", publish_date = "2022-02-15", multiplayer, last_played_at)
+        super(genre, author, source, label, publish_date)
         @multiplayer = multiplayer
         @last_played_at = Date.parse(last_played_at)
     end
@@ -13,9 +14,7 @@ class Game < Item
         current_year = DateTime.now
         current_year = current_date.strftime "%Y"
         age = current_year - last_played_at.year
-        if super && age > 2
-            return true
-        end
-        return false
+        return true if super && age > 2
+        false
     end
 end
