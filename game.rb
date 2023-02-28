d24971f7cfe817f38ef57e7839942be4dee5e381
enoqueJonas <enoque.macanda@gmail.com>
2023-02-28 17:21:33 +0200

    ADD can_be_archived? method in game.rb

--- game.rb
+++ game.rb
@@ -1,8 +1,21 @@
-class Game
+require_relative './item.rb'
+require 'date'
+
+class Game < Item
     attr_accessor :multiplayer, :last_played_at
-    
+
     def initialize(multiplayer, last_played_at)
         @multiplayer = multiplayer
-        @last_played_at = last_played_at
+        @last_played_at = Date.parse(last_played_at)
+    end
+
+    def can_be_archived?
+        current_year = DateTime.now
+        current_year = current_date.strftime "%Y"
+        age = current_year - last_played_at.year
+        if super && age > 2
+            return true
+        end
+        return false
     end
 end
