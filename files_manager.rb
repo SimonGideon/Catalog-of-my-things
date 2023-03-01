055c718acf629cdb6866704cf45c27e1f16316cd
enoqueJonas <enoque.macanda@gmail.com>
2023-03-01 19:38:44 +0200

    ADD FileManager#read_on_file

--- files_manager.rb
+++ files_manager.rb
@@ -3,4 +3,9 @@ Class FileManager
 		json = JSON.generate(data)
 		file.write(file_name, json, mode: ‘w’)
 	end
+    def read_on_file(file_name)
+		write_on_file(file_name, []) unless File.exist?(file_name)
+		data = JSON.parse(file.read(file_name), { symbolize_names: true })
+		data
+	end
 end
